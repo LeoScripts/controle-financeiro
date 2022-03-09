e9a95d46716bbe2a1a8c4e239812b1a501c0e1fb
Leandro Cavalcante <leandroguitarjesus89@gmail.com>
2022-03-09 14:51:40 -0300

    stilizaçao inicial do header e planejamento do body do projeto

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,17 @@ import * as C from "./App.styles";
 export function App() {
   return (
     <C.Container>
-      <h1>App</h1>
+      <C.Header>
+        <C.HeaderText>Controle Financeiro</C.HeaderText>
+      </C.Header>
+
+      <C.Body>
+        {/* area de informações */}
+
+        {/* area de incersão de dados */}
+
+        {/* tabela de intens */}
+      </C.Body>
     </C.Container>
   );
 }
