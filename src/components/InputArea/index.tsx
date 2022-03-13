b34132441181a804c346e02600d8730dd7ffcc19
Leandro Cavalcante <leandroguitarjesus89@gmail.com>
2022-03-13 18:45:10 -0300

    estilizaçao buton e date

--- src/components/InputArea/index.tsx
+++ src/components/InputArea/index.tsx
@@ -1,28 +1,34 @@
-import * as C from './styles';
-import { Item } from '../../types/Item';
+import * as C from "./styles";
+import { Item } from "../../types/Item";
+import { useState } from "react";
 
 type Props = {
-    onAdd: (item: Item) => void;
-}
+  onAdd: (item: Item) => void;
+};
 
-export const InputArea = ({onAdd}: Props) => {
-    const handleAddEvent = () => {
+export const InputArea = ({ onAdd }: Props) => {
+  const handleAddEvent = () => {
+    // item fixo so pra teste
+    let newItem: Item = {
+      date: new Date(),
+      category: "foot",
+      title: "Item de teste",
+      value: 250.25,
+    };
 
-        // item fixo so pra teste
-        let newItem: Item = {
-            date: new Date(2022, 2, 25),
-            category: 'rend',
-            title: 'Item de teste',
-            value: 250.25
+    
 
-        }
-        
-        onAdd(newItem);
-    }
+    onAdd(newItem);
+  };
 
-    return (
-        <C.Container>
-            <button onClick={handleAddEvent}>Adicionar</button>
-        </C.Container>
-    )
-}
+  return (
+    <C.Container>
+      <C.Date>
+        <label htmlFor="date">Date</label>
+        <input onChange={(e)=> {e.target.value}} type="date" name="date" id="date" />
+      </C.Date>
+
+      <button onClick={handleAddEvent}>Adicionar</button>
+    </C.Container>
+  );
+};
--- src/components/InputArea/styles.ts
+++ src/components/InputArea/styles.ts
@@ -6,4 +6,42 @@ export const Container = styled.div`
     border-radius: 10px;
     padding: 20px;
     margin-top: 20px;
+
+    button{
+        width: 100%;
+        padding: 10px;
+        border-radius: 10px;
+        border: none;
+        box-shadow: 0 0 10px 0 gray;
+        background: blue;
+        color: #FFF;
+        font-weight: bold;
+        margin-top: 10px;
+    }
+
+    button:hover{
+        opacity: 0.8;
+    }
+    button:active{
+        opacity: 1;
+    }
+`;
+
+export const Date = styled.div`
+    padding:20px;
+    display:flex;
+    flex-direction:column;
+    width: 25%;
+    border-right: solid  3px gray;
+
+    label{
+        text-align: center;
+        font-weight: bold;
+    }
+
+    input{
+        padding: 5px;
+        margin-top: 10px;
+        border-radius: 5px;
+    }
 `;
